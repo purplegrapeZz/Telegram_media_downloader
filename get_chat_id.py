aaeb85361733f2ca92745ae05b9c14124f6175b0
Necromancer_ <1243184128@qq.com>
2024-01-03 16:44:54 +0800

    Update New Version

--- get_chat_id.py
+++ get_chat_id.py
@@ -8,12 +8,12 @@ app = Client(
 	api_id = cfg.API_ID,
 	api_hash = cfg.API_HASH,
 	phone_number = cfg.PHONE_NUMBER,
-	# proxy = dict(scheme = 'socks5', hostname = cfg.HOST, port = cfg.PORT),
+	# proxy = dict(scheme = 'socks5', hostname = cfg.HOST,port = cfg.PORT),
 	# bot_token = cfg.BOT_TOKEN,
 )
 async def main():
 	await app.start()
-	async for dialog in app.iter_dialogs():
+	async for dialog in app.get_dialogs():
 		print((dialog.chat.first_name+' '+dialog.chat.last_name if dialog.chat.last_name else dialog.chat.first_name)or dialog.chat.title, dialog.chat.id)
 
 app.run(main())
--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import cfg
 
 
 app = Client(
-	session_name = 'user',
+	name = 'user',
 	api_id = cfg.API_ID,
 	api_hash = cfg.API_HASH,
 	phone_number = cfg.PHONE_NUMBER,
-	# proxy = dict(hostname = cfg.HOST,port = cfg.PORT),
+	# proxy = dict(scheme = 'socks5', hostname = cfg.HOST,port = cfg.PORT),
 	# bot_token = cfg.BOT_TOKEN,
 )
 target = cfg.TARGET
@@ -19,22 +19,24 @@ target = cfg.TARGET
 PATH = os.path.dirname(os.path.abspath(__file__))+'/downloads/'
 
 async def main():
+	file_name = ''
 	await app.start()
-	async for msg in app.iter_history(target):
+	async for msg in app.get_chat_history(target):
 		try:
 			if msg.photo and cfg.opts[1]=='photo':
-				date = dt.utcfromtimestamp(msg.photo.date)
+				date = msg.photo.date
 				file = 'photo_'+str(date)[:10]+'_'+msg.photo.file_unique_id+'.jpg'		
 			elif msg.video and cfg.opts[2]=='video':
-				date = dt.utcfromtimestamp(msg.video.date)
+				date = msg.video.date
 				file = 'video_'+str(date)[:10]+'_'+msg.video.file_unique_id+msg.video.file_name
 			elif msg.document and cfg.opts[3]=='document':
-				date = dt.utcfromtimestamp(msg.document.date)
+				date = msg.document.date
 				file = 'file_'+str(date)[:10]+'_'+msg.document.file_unique_id+msg.document.file_name
 			
 			if not os.path.exists(PATH+file):
 				print(await app.download_media(msg,file_name=file))
-		except:
+		except Exception as e:
+			# print(e)
 			continue
 
 app.run(main())
